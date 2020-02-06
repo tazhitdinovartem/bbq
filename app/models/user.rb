abb99101ca2b65f5f082b2cf166e3b9f74b15212
Artem Tazhitdinov <nemesisrise@gmail.com>
2020-02-06 14:40:17 +0500

    Added User and Event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  
+  EMAIL_VALIDATION_REGEXP = /.+@.+\..+/i
+  has_many :events
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, uniqueness: true, presence: true, length: { maximum: 100 }, format: { with: EMAIL_VALIDATION_REGEXP }
 end
