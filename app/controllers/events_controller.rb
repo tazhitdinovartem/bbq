2317ea0ed09d03122918896c1989c76163e786a8
artem <tazhitdinov.artem@gmail.com>
2020-02-08 18:10:19 +0500

    Simple form added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -74,7 +74,7 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :address, :datetime, :desciption)
+      params.require(:event).permit(:title, :address, :datetime, :description)
     end
 
     def set_current_user_event
