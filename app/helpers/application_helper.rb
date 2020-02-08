91945c37da199f607482ea5306fc7787d947bd7f
artem <tazhitdinov.artem@gmail.com>
2020-02-08 13:32:12 +0500

    Added styles for signup and signin forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def user_avatar(user)
     asset_path('user.png')
   end
+
+  def flash_class(level)
+    case level
+      when 'info' then "alert alert-info"
+      when 'notice','success' then "alert alert-success"
+      when 'error' then "alert alert-danger"
+      when 'alert' then "alert alert-warning"
+    end
+  end
 end
