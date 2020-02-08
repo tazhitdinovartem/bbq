b8b7a730917f51d86e4251b3df05f1b25f5ac545
Artem Tazhitdinov <nemesisrise@gmail.com>
2020-02-08 20:26:05 +0500

    Styles fixed for simple form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,33 +1,22 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-
-  # Задаем объект @event для экшена show
   before_action :set_event, only: [:show]
-
-  # Задаем объект @event от текущего юзера для других действий
   before_action :set_current_user_event, only: [:edit, :update, :destroy]
-  # GET /events
-  # GET /events.json
+  
   def index
     @events = Event.all
   end
 
-  # GET /events/1
-  # GET /events/1.json
   def show
   end
 
-  # GET /events/new
   def new
     @event = current_user.events.build
   end
 
-  # GET /events/1/edit
   def edit
   end
 
-  # POST /events
-  # POST /events.json
   def create
     @event = current_user.events.build(event_params)
 
@@ -42,37 +31,28 @@ class EventsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
     respond_to do |format|
       if @event.update(event_params)
         format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
     respond_to do |format|
       format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
       params.require(:event).permit(:title, :address, :datetime, :description)
     end
