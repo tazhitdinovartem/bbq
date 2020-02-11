1041a8ec106ea3f85d6b5608953763cfec2cab2c
Artem Tazhitdinov <nemesisrise@gmail.com>
2020-02-11 18:52:50 +0500

    Testing datepicker with webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,9 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("packs/main")
+require("packs/datetime.js")
+require("packs/datetimeru.js")
+require("packs/script.js")
 require("bootstrap/dist/js/bootstrap")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
