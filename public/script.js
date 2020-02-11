872420134e519fc6f022a23a0b19654d73b13fcc
Artem Tazhitdinov <nemesisrise@gmail.com>
2020-02-11 15:45:59 +0500

    Added datetimepicker

---
+++ public/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+  tail.DateTime("#event_datetime", {
+    locale: 'ru',
+    position: 'right',
+    stayOpen: true,
+    timeHours: 24,
+    timeMinutes: 00,
+    weekStart: 1
+  });
+});
+
--- public/test.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-  tail.DateTime("#datepicker", {
-    locale: 'ru'
-  });
-});
