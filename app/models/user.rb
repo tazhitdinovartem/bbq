b82e04ae19dabb62c0f83878424a68149f986aae
artem <tazhitdinov.artem@gmail.com>
2020-02-13 14:03:13 +0500

    Adding comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events, dependent: :destroy
-
+  has_many :comments, dependent: :destroy
+  
   validates :name, presence: true, length: { maximum: 35 }
   validates :email, uniqueness: true, presence: true, length: { maximum: 100 }
 
-  #before_validation :set_name, on: :create
+  before_validation :set_name, on: :create
 
-  #private
+  private
 
-  #def set_name
-    #self.name = "User №#{rand(777)}" if self.name.blank?
-  #end
+  def set_name
+    self.name = "User №#{rand(777)}" if self.name.blank?
+  end
 end
