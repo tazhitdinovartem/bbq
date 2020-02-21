eae9c0cde5ec90693fbeadf9740878dd57362edc
Artem Tazhitdinov <nemesisrise@gmail.com>
2020-02-21 13:48:22 +0500

    Added gem letter_opener for dev-envoirnment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,13 +62,5 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: '587',
-    user_name: 'bbqpartyapp@gmail.com',
-    authentication: 'plain',
-    enable_starttls_auto: true
-}
+  config.action_mailer.delivery_method = :letter_opener
 end
