f4a4a69b28c8e53c8c9b928a26b841a061a5794a
Artem Tazhitdinov <nemesisrise@gmail.com>
2020-02-21 13:03:11 +0500

    Fixed devise config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
-    user_name: 'artemrails978',
+    user_name: 'bbqpartyapp@gmail.com',
     authentication: 'plain',
     enable_starttls_auto: true
 }
