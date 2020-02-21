3c4d6cacd400fdeac3d78d8d609c71b4c902d9a3
ArtemTazhitdinov <tazhitdinov.artem@gmail.com@>
2020-02-21 21:34:20 +0500

    Author of comment will not get notice now

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -44,6 +44,10 @@ class CommentsController < ApplicationController
   def notify_subscribers(event, comment)
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
 
+    if current_user.present?
+      all_emails -= [current_user.email]
+    end
+    
     all_emails.each do |mail|
       EventMailer.comment(event, comment, mail).deliver_now
     end
