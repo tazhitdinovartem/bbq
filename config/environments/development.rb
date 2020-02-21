8136609ee1062cad741c6c84f64ac8cbe960e3a6
ArtemTazhitdinov <tazhitdinov.artem@gmail.com@>
2020-02-21 09:47:57 +0500

    Delete password from config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
-    user_name: 'artemrails978', # не используйте для тестов свои реальные ящики
+    user_name: 'artemrails978',
+    password: 'server88',
     authentication: 'plain',
     enable_starttls_auto: true
 }
