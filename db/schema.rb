ad20a47bde8f9f5d7ff89f4cbb61d711db6e493d
ArtemTazhitdinov <tazhitdinov.artem@gmail.com@>
2020-03-02 20:25:11 +0500

    Added pin-code for events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_073159) do
+ActiveRecord::Schema.define(version: 2020_03_02_145715) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_073159) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
