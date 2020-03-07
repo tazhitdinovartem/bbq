54be680d6d3ca4d9b4d95c5e0c5193eb4944b5a1
ArtemTazhitdinov <tazhitdinov.artem@gmail.com@>
2020-03-07 15:42:24 +0500

    Trying to fix deploy bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,8 +119,8 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     port: '587',
     authentication: 'plain',
-    user_name: Rails.application.credentials.sendgrid[:username],
-    password: Rails.application.credentials.sendgrid[:password],
+    #user_name: Rails.application.credentials.sendgrid[:username],
+    #password: Rails.application.credentials.sendgrid[:password],
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
