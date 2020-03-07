6620559e1ac5ebd4dbdd0ae244d309d682d7c425
ArtemTazhitdinov <tazhitdinov.artem@gmail.com@>
2020-03-07 15:48:53 +0500

    Fixed capistrano deploy error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,12 +4,12 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      #provider:              'AWS',
-      #region:                Rails.application.credentials.aws[:bucket_region], 
-      #aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],
-      #aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
+      provider:              'AWS',
+      region:                Rails.application.credentials.aws[:bucket_region], 
+      aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
     }
 
-    #config.fog_directory  = Rails.application.credentials.aws[:bucket_name]
+    config.fog_directory  = Rails.application.credentials.aws[:bucket_name]
   end
 end
