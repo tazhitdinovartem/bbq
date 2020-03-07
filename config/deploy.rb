306490bd0c1ed92679b3dd6706b8f465c25dd6c9
ArtemTazhitdinov <tazhitdinov.artem@gmail.com@>
2020-03-07 17:14:16 +0500

    Fix for deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/deploy/bbq"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", %w{config/master.key}
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
