6bd705193f0f23341548f0a8c06f38b5dec7ac76
ArtemTazhitdinov <tazhitdinov.artem@gmail.com@>
2020-03-20 23:58:18 +0500

    Fixed tests

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe EventPolicy do
   let(:user) { FactoryBot.create(:user) }
+  let(:another_user) { FactoryBot.create(:user) }
   let(:event) { FactoryBot.create(:event, user: user) }
 
   subject { EventPolicy }
@@ -13,6 +14,12 @@ RSpec.describe EventPolicy do
   end
 
   context 'user is not event owner' do
+    permissions :edit?, :update?, :destroy? do
+      it { is_expected.not_to permit(another_user, event) }
+    end
+  end
+
+  context 'anonimous user cant edit/update/destroy event' do
     permissions :edit?, :update?, :destroy? do
       it { is_expected.not_to permit(nil, event) }
     end
